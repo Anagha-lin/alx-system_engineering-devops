51ec65803a022fa57128b6ffb9ed10177a2b84b2
Anagha-lin <linusanagha37@gmail.com>
2024-04-09 05:11:11 -0700

    a

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+0-subs
+"""
+import requests
+
+def number_of_subscribers(subreddit):
+    """
+    Returns the number of subscribers for a given subreddit.
+    If the subreddit is invalid, returns 0.
+    """
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0'}  # Set a custom User-Agent to prevent errors
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        return data['data']['subscribers']
+    else:
+        return 0
+
+if __name__ == "__main__":
+    print(number_of_subscribers("programming"))
+
