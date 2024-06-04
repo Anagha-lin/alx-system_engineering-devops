cb438406758e5b1a42addee0332f98b03d5af453
Anagha-lin <linusanagha37@gmail.com>
2024-06-04 10:27:45 +0000

    a

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Returns the number of subscribers for a given subreddit
+"""
+
+import requests
+
+def number_of_subscribers(subreddit):
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'MyAPI/0.0.1'}
+    
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        return data['data']['subscribers']
+    else:
+        return 0
+
+if __name__ == "__main__":
+    subreddit = input("Enter the subreddit: ")
+    print(number_of_subscribers(subreddit))
+
