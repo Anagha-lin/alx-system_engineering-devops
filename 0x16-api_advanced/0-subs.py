106fdfdafb3bd3cb282d7c95967d42f8cf9cc740
Anagha-lin <linusanagha37@gmail.com>
2024-06-04 17:05:29 +0000

    a

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,7 +6,7 @@ Returns the number of subscribers for a given subreddit
 import requests
 
 def number_of_subscribers(subreddit):
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {'User-Agent': 'MyAPI/0.0.1'}
     
     response = requests.get(url, headers=headers, allow_redirects=False)
